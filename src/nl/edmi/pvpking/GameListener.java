2fd0fcb5a47456a909b5385c16bdf2587b51c50a
Vescatur <ivan@edmi.nl>
2016-09-02 12:27:10 +0200

    No comment

--- src/nl/edmi/pvpking/GameListener.java
+++ src/nl/edmi/pvpking/GameListener.java
@@ -28,14 +28,21 @@ public class GameListener implements Listener {
     public GameListener(Main instance){
         main = instance;
     }
+
     @EventHandler(priority = EventPriority.NORMAL)
     public void onEntityDeath(PlayerDeathEvent e){
         if (e.getEntity() instanceof Player){
             Player player = (Player) e.getEntity();
-
+            player.sendMessage("a");
+            if(Main.game.PlayersAlive.contains(player)) {
+                player.sendMessage("b");
+                Main.game.PlayersAlive.remove(player);
+            }
+            player.sendMessage("c");
         }
     }
 
+    @EventHandler(priority = EventPriority.NORMAL)
     public void onPlayerJoin(PlayerJoinEvent e){
         if(Main.game.battle) {
             Player player = e.getPlayer();
@@ -45,20 +52,29 @@ public class GameListener implements Listener {
         }
     }
 
+    @EventHandler(priority = EventPriority.NORMAL)
     public void onPlayerQuit(PlayerQuitEvent e) {
         if(Main.game.battle) {
-            if(Main.game.PlayersAlive.contains(e.getPlayer())) {
-                Main.game.PlayersAlive.remove(e.getPlayer());
+            Player player = (Player) e.getPlayer();
+            player.sendMessage("a");
+            if(Main.game.PlayersAlive.contains(player)) {
+                player.sendMessage("b");
+                Main.game.PlayersAlive.remove(player);
             }
+            player.sendMessage("c");
         }
     }
 
+    @EventHandler(priority = EventPriority.NORMAL)
     public void onPlayerRespawn(PlayerRespawnEvent e) {
         if(Main.game.battle) {
             Player player = e.getPlayer();
+            player.sendMessage("a");
             if(Main.game.PlayersAlive.contains(player)) {
+                player.sendMessage("b");
                 Main.game.PlayersAlive.remove(player);
             }
+            player.sendMessage("c");
             World world = Bukkit.getWorlds().get(5);
             Location loc = new Location(world,0,140,0);
             player.teleport(loc);
